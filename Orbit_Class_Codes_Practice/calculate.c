da136cf07ec05ad6463d660b1a9e917028652f84
Amit Ghatage <amitghatage001@gmail.com>
2024-02-12 16:15:56 +0530

    DRCP Logic way of programming

---
+++ Orbit_Class_Codes_Practice/calculate.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    // Declare 
+    int a;
+    int b;
+
+    // Read 
+    printf("Enter a\n");
+    scanf("%d",&a);
+    printf("Enter b\n");
+    scanf("%d",&b);
+
+    //calculate
+    int ans=a+b;
+
+    //print result
+    printf("Answer is %d",ans);
+}
---
+++ start.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int age;
+    scanf("Enter Your age");
+    scanf("%d", &age);
+    printf("Hello World\n");
+    printf("age is: %d",age);
+}
