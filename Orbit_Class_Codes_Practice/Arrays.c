85c509be49b22864ef87c798b97efa689d4d21b3
Amit Ghatage <amitghatage001@gmail.com>
2024-04-15 12:59:00 +0530

    Break and Continue keywords

--- Orbit_Class_Codes_Practice/Arrays.c
+++ Orbit_Class_Codes_Practice/Arrays.c
@@ -4,7 +4,7 @@ int main(){
     //intializing the array
     printf("Enter elements into the array\n");
     for(int i=0;i<10;i++){
-        printf("Enter the elemet\n");
+        printf("Enter the element\n");
         scanf("%d",&arr[i]);
     }
     for(int i=0;i<10;i++){
