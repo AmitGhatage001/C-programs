121e065c67c7aaa5ec0e39d885ea32e3196ad1b9
Amit Ghatage <amitghatage001@gmail.com>
2024-07-16 15:05:24 +0530

    DRCP logical way to solve problems

---
+++ Orbit_Class_Codes_Practice/Logic.Buidling/DRCP/add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    //Declare 
+    int a;
+    int b;
+
+    //Read
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    //Calculate 
+    int ans = a + b;
+
+    //Print
+    printf("Result is :%d",ans);
+}
